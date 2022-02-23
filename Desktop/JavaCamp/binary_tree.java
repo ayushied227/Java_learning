049a14004071a9f0d88a5def0ae6d64ee24e7afc
AYUSHI DUFARE <ayushidufare22@gmail.com>
2022-02-23 21:55:44 +0530

    first commit

---
+++ Desktop/JavaCamp/binary_tree.java
@@ -0,0 +1,62 @@
+class Node{
+    Node left, right;
+    int data;
+    public Node(int data){
+        this.data=data;
+    }
+    public void insert(int value){
+        if (value<=data){
+            if(left==null){
+                left=new Node(value);
+            }
+            else{
+                left.insert(value);
+            }
+        }
+        else{
+            if (right==null){
+                right=new Node (value);
+            }
+            else{
+                right.insert(value);
+            }
+        }
+    }
+    public boolean contains(int value){
+        if (value==data){
+            return true;
+        }
+        else{
+            if (value<data){
+                if (left==null){
+                    return false
+                }
+                else{
+                    return left.contains(value);
+                }
+            }
+            else{
+                if (right==null){
+                    return false
+                }
+                else{
+                    return right.contains(value);
+                }
+            }
+        }
+    }
+    public void printInOrder(){
+        if (left!=null){
+            left.printInOrder();
+        }
+        System.out.println(data);
+        if (right!=null){
+            right.printInOrder();
+        }
+        System.out.println(data);
+    }
+}
+
+// in-order 1-2-3(right way: 2 as parent node, 1 as left node, 3 as right node )
+// pre-order 2-1-3
+// post-order 1-3-2
